5e8ebe00d9ef62b9d90616c7f0c3e119c43386ab
Anton Eliseev <anton_saintp@mail.ru>
2023-11-29 18:46:33 +0300

    apiKey

--- routes/dalle.routes.js
+++ routes/dalle.routes.js
@@ -7,7 +7,7 @@ dotenv.config();
 const router = express.Router();
 
 const config = new Configuration({
-  apiKey: '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',
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(config);
